665a8ad5d95c9495d61b7c66db77ed3b36a088ef
Alastair Longmuir <alastairlinwood@gmail.com>
2024-02-22 14:22:27 +0100

    css for filter

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState, useEffect } from "react";
 
 function FilterSection({ changeSearchState, changeCheckedState }) {
   const handleSearchChange = (e) => {
